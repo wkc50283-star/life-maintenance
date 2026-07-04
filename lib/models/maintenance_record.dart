ea398a8d19d6be9bf66e872300befe86cf413215
wangkuocheng <wkc50283@gmail.com>
2026-07-04 22:43:30 +0800

    Add maintenance record JSON serialization

--- lib/models/maintenance_record.dart
+++ lib/models/maintenance_record.dart
@@ -37,6 +37,50 @@ class MaintenanceRecord {
     this.note,
   });
 
+  factory MaintenanceRecord.fromJson(Map<String, dynamic> json) {
+    return MaintenanceRecord(
+      id: json['id'] as String,
+      itemId: json['itemId'] as String,
+      taskId: json['taskId'] as String?,
+      recordType: RecordType.values.byName(json['recordType'] as String),
+      date: DateTime.parse(json['date'] as String),
+      title: json['title'] as String,
+      issueDescription: json['issueDescription'] as String?,
+      workDescription: json['workDescription'] as String?,
+      partsChanged: List<String>.from(json['partsChanged'] as List<dynamic>),
+      cost: json['cost'] as int?,
+      vendorName: json['vendorName'] as String?,
+      warrantyUntil: json['warrantyUntil'] == null
+          ? null
+          : DateTime.parse(json['warrantyUntil'] as String),
+      result: json['result'] as String?,
+      photos: List<String>.from(json['photos'] as List<dynamic>),
+      note: json['note'] as String?,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'itemId': itemId,
+      'taskId': taskId,
+      'recordType': recordType.name,
+      'date': date.toIso8601String(),
+      'title': title,
+      'issueDescription': issueDescription,
+      'workDescription': workDescription,
+      'partsChanged': partsChanged,
+      'cost': cost,
+      'vendorName': vendorName,
+      'warrantyUntil': warrantyUntil?.toIso8601String(),
+      'result': result,
+      'photos': photos,
+      'note': note,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
   MaintenanceRecord copyWith({
     String? id,
     String? itemId,
