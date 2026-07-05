e99e967e01c1829b0d9f5426741bced15579fcfb
wangkuocheng <wkc50283@gmail.com>
2026-07-06 07:53:43 +0800

    Add task local repository

---
+++ lib/repositories/task_local_repository.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import '../models/task.dart';
+import '../services/local_storage_service.dart';
+
+class TaskLocalRepository {
+  static const String _storageKey = 'tasks';
+
+  TaskLocalRepository(this._storageService);
+
+  final LocalStorageService _storageService;
+
+  Future<List<Task>> loadTasks() async {
+    final rawTasks = await _storageService.readString(_storageKey);
+    if (rawTasks == null) {
+      return <Task>[];
+    }
+
+    final decodedTasks = jsonDecode(rawTasks) as List<dynamic>;
+    return decodedTasks
+        .map((task) => Task.fromJson(task as Map<String, dynamic>))
+        .toList();
+  }
+
+  Future<void> saveTasks(List<Task> tasks) async {
+    final encodedTasks = jsonEncode(
+      tasks.map((task) => task.toJson()).toList(),
+    );
+    await _storageService.saveString(_storageKey, encodedTasks);
+  }
+}
