4e5f109ec0f3f2579fef882e36ebc764b6a138e2
wkc50283-star <wkc50283@gmail.com>
2026-07-17 22:59:20 +0800

    Protect task storage from partial-read overwrite

--- lib/repositories/task_local_repository.dart
+++ lib/repositories/task_local_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import '../models/task.dart';
+import '../services/local_data_integrity_service.dart';
 import '../services/local_storage_service.dart';
 
 class TaskLocalRepository {
@@ -13,20 +14,19 @@ class TaskLocalRepository {
   Future<List<Task>> loadTasks() async {
     final rawTasks = await _storageService.readString(_storageKey);
     if (rawTasks == null) {
+      LocalDataIntegrityService.instance.clearIssue(_storageKey);
       return <Task>[];
     }
 
-    try {
-      final decodedTasks = jsonDecode(rawTasks) as List<dynamic>;
-      return decodedTasks
-          .map((task) => Task.fromJson(task as Map<String, dynamic>))
-          .toList();
-    } catch (_) {
-      return <Task>[];
-    }
+    return LocalDataIntegrityService.instance.decodeList<Task>(
+      storageKey: _storageKey,
+      rawValue: rawTasks,
+      decodeEntry: Task.fromJson,
+    );
   }
 
   Future<void> saveTasks(List<Task> tasks) async {
+    LocalDataIntegrityService.instance.ensureWritesAllowed();
     final encodedTasks = jsonEncode(
       tasks.map((task) => task.toJson()).toList(),
     );
